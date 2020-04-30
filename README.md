4db9c89f4f249e9c9de27a100735dc6d317e57c3
Satoshi Konno <skonno@cybergarage.org>
2020-04-30 21:35:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# go-mysql
+
+`go-mysql` is an open-source framework that conforms to the standard specification of [MySQL](https://www.mysql.com/), and it is developed to be able to implement the compatible distributed database server easily.
---
+++ mysql/config.go
@@ -0,0 +1,49 @@
+// Copyright (C) 2020 Satoshi Konno. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mysql
+
+const (
+	defaultPort = 3306
+)
+
+// Config stores server configuration parammeters.
+type Config struct {
+	Addr string
+	Port int
+}
+
+// NewDefaultConfig returns a default configuration instance.
+func NewDefaultConfig() *Config {
+	config := &Config{
+		Addr: "",
+		Port: defaultPort,
+	}
+	return config
+}
+
+// SetAddress sets a bind interface address.
+func (config *Config) SetAddress(addr string) {
+	config.Addr = addr
+}
+
+// SetPort sets a listen port.
+func (config *Config) SetPort(port int) {
+	config.Port = port
+}
+
+// GetPort returns a listent port.
+func (config *Config) GetPort() int {
+	return config.Port
+}
