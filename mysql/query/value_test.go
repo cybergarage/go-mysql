3855a18c0709d93786d86b199078139e8a97ecfb
Satoshi Konno <skonno@cybergarage.org>
2020-09-04 16:58:43 +0900

    Add NewValueWithSQLVal()

---
+++ mysql/query/value_test.go
@@ -0,0 +1,45 @@
+// Copyright (C) 2020 The go-mysql Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package query
+
+import (
+	"testing"
+
+	vitess "vitess.io/vitess/go/vt/sqlparser"
+)
+
+func TestNewValue(t *testing.T) {
+	sqlValues := []*vitess.SQLVal{
+		vitess.NewStrVal([]byte("hello")),
+		vitess.NewIntVal([]byte("1234")),
+		vitess.NewFloatVal([]byte("1234")),
+	}
+	expValues := []interface{}{
+		"hello",
+		int64(1234),
+		float64(1234),
+	}
+
+	for n, sqlVal := range sqlValues {
+		val, err := NewValueWithSQLVal(sqlVal)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if val != expValues[n] {
+			t.Errorf("%s != %s", val, expValues[n])
+		}
+	}
+}
