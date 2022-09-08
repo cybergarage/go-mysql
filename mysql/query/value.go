3312594a508aea0ed67b7e9b05b31e572b24c33c
Satoshi Konno <skonno@cybergarage.org>
2022-09-08 09:57:55 +0900

    Define types

--- mysql/query/value.go
+++ mysql/query/value.go
@@ -18,29 +18,164 @@ import (
 	"fmt"
 	"strconv"
 
-	vitess "vitess.io/vitess/go/vt/sqlparser"
+	"vitess.io/vitess/go/sqltypes"
+	vitesstypes "vitess.io/vitess/go/sqltypes"
+	vitesspb "vitess.io/vitess/go/vt/proto/query"
+	vitesssql "vitess.io/vitess/go/vt/sqlparser"
 )
 
 const (
-	StrVal   = vitess.StrVal
-	IntVal   = vitess.IntVal
-	FloatVal = vitess.FloatVal
-	HexNum   = vitess.HexNum
-	HexVal   = vitess.HexVal
-	BitVal   = vitess.BitVal
+	StrVal   = vitesssql.StrVal
+	IntVal   = vitesssql.IntVal
+	FloatVal = vitesssql.FloatVal
+	HexNum   = vitesssql.HexNum
+	HexVal   = vitesssql.HexVal
+	BitVal   = vitesssql.BitVal
 )
 
-// NewValueWithLiteral converts the specified literal to Golang value.
-func NewValueWithLiteral(literal *vitess.Literal) (interface{}, error) {
-	switch literal.Type {
-	case StrVal:
-		return literal.Val, nil
-	case IntVal:
-		return strconv.ParseInt(literal.Val, 0, 64)
-	case FloatVal:
-		return strconv.ParseFloat(literal.Val, 64)
-	case BitVal, HexNum, HexVal:
-		return nil, fmt.Errorf(errorUnknownSQLValType, literal)
+// SQLVal represents a single value.
+type SQLVal = vitesstypes.Value
+
+// ValType specifies the type for SQLVal.
+type ValType = vitesspb.Type
+
+// Value represents a query value.
+type Value struct {
+	typ   ValType
+	value interface{}
+}
+
+// Expr represents an expression.
+type Expr = vitesssql.Expr
+
+// ValTuple represents a tuple of actual values.
+type ValTuple = vitesssql.ValTuple
+
+// ColName represents a column name.
+type ColName = vitesssql.ColName
+
+// AndExpr represents an AND expression.
+type AndExpr = vitesssql.AndExpr
+
+// OrExpr represents an OR expression.
+type OrExpr = vitesssql.OrExpr
+
+// XorExpr represents an XOR expression.
+type XorExpr = vitesssql.XorExpr
+
+// NotExpr represents a NOT expression.
+type NotExpr = vitesssql.NotExpr
+
+// ComparisonExpr represents a two-value comparison expression.
+type ComparisonExpr = vitesssql.ComparisonExpr
+
+// NewValue creates a query value .
+func NewValue() *Value {
+	return &Value{}
+}
+
+// NewValueWithType creates a query value with the specified type.
+func NewValueWithType(val interface{}, valType ValType) *Value {
+	return &Value{
+		typ:   valType,
+		value: val,
 	}
-	return nil, fmt.Errorf(errorUnknownSQLValType, literal)
+}
+
+// NewValueWithValue creates a query value from the raw SQL value.
+func NewValueWithValue(val interface{}) (*Value, error) {
+	value := NewValue()
+	return value, value.SetValue(val)
+}
+
+// SetType sets a value type.
+func (value *Value) SetType(t ValType) {
+	value.typ = t
+}
+
+// Type returns the type.
+func (value *Value) Type() ValType {
+	return value.typ
+}
+
+// SetValue sets a value.
+func (value *Value) SetValue(val interface{}) error {
+	switch v := val.(type) {
+	case SQLVal:
+		return value.setSQLValue(v)
+	case *Literal:
+		return value.setLiteralValue(v)
+	}
+
+	value.value = val
+
+	return nil
+}
+
+// setLiteralValue sets a literal value and type by the specified value, and
+// returns SetLiteralValue error when the specified value is unknown.
+// See: vitess.io/vitess/go/sqltypes::NewValue().
+func (value *Value) setLiteralValue(lv *Literal) error {
+	var v interface{}
+	var vt vitesspb.Type
+	var err error
+
+	switch lv.Type {
+	case StrVal:
+		vt = sqltypes.VarBinary
+		v = string(lv.Val)
+	case IntVal:
+		vt = sqltypes.Int64
+		v, err = strconv.ParseInt(string(lv.Val), 0, 64)
+	case FloatVal:
+		vt = sqltypes.Float64
+		v, err = strconv.ParseFloat(string(lv.Val), 64)
+	default:
+		err = fmt.Errorf(errorLiteralUnknownType, lv)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	value.typ = vt
+	value.value = v
+
+	return nil
+}
+
+// setSQLValue sets a query value and type by the specified value, and
+// returns an error when the specified value is unknown.
+// See: vitess.io/vitess/go/sqltypes::NewValue().
+func (value *Value) setSQLValue(sv SQLVal) error {
+	var v interface{}
+	var err error
+
+	vt := sv.Type()
+	switch {
+	case vitesstypes.IsSigned(vt):
+		v, err = strconv.ParseInt(string(sv.Raw()), 0, 64)
+	case vitesstypes.IsUnsigned(vt):
+		v, err = strconv.ParseUint(string(sv.Raw()), 0, 64)
+	case vitesstypes.IsFloat(vt) || vt == Decimal:
+		v, err = strconv.ParseFloat(string(sv.Raw()), 64)
+	case vitesstypes.IsQuoted(vt) || vt == Bit || vt == Null:
+		v = string(sv.Raw())
+	default:
+		return fmt.Errorf(errorColumnUnexpectedType, sv.Raw(), sv.Type())
+	}
+
+	if err != nil {
+		return err
+	}
+
+	value.typ = vt
+	value.value = v
+
+	return nil
+}
+
+// Value returns the value.
+func (value *Value) Value() interface{} {
+	return value.value
 }
