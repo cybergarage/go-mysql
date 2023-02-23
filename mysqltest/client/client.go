0d81d8150113d121a8957f52fd3728b2e0921461
Satoshi Konno <skonno@cybergarage.org>
2023-02-23 18:20:37 +0900

    Fix sqlclosecheck warnings

--- mysqltest/client/client.go
+++ mysqltest/client/client.go
@@ -36,15 +36,23 @@ func NewDefaultClient() *Client {
 // CreateDatabase creates a specified database.
 func (client *Client) CreateDatabase(name string) error {
 	query := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", name)
-	_, err := client.Query(query)
-	return err
+	rows, err := client.Query(query)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	return nil
 }
 
 // DropDatabase dtops a specified database.
 func (client *Client) DropDatabase(name string) error {
 	query := fmt.Sprintf("DROP DATABASE IF EXISTS %s", name)
-	_, err := client.Query(query)
-	return err
+	rows, err := client.Query(query)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	return nil
 }
 
 // Use sets a target database.
