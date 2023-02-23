335de79b4ac671807ab882f4322233a447861ca3
Satoshi Konno <skonno@cybergarage.org>
2023-02-23 17:29:42 +0900

    Update Clinet::Open()

--- mysql/client.go
+++ mysql/client.go
@@ -38,7 +38,7 @@ func NewClient() *Client {
 
 // Open opens a database specified by the internal configuration.
 func (client *Client) Open() error {
-	dsName := fmt.Sprintf("root@tcp(%s:%d)/%s", client.Host, client.Port, client.Database)
+	dsName := fmt.Sprintf("tcp(%s:%d)/%s", client.Host, client.Port, client.Database)
 	db, err := sql.Open("mysql", dsName)
 	if err != nil {
 		return err
