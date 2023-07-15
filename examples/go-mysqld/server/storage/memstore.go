9eb88d25b5959a168fcc23adf12df564d711ae1a
Satoshi Konno <skonno@cybergarage.org>
2023-07-15 20:32:56 +0900

    Update storage.NewTableWith()

--- examples/go-mysqld/server/storage/memstore.go
+++ examples/go-mysqld/server/storage/memstore.go
@@ -82,7 +82,7 @@ func (store *MemStore) CreateTable(conn *mysql.Conn, stmt *query.Schema) (*mysql
 	tableName := stmt.TableName()
 	_, ok = db.GetTable(tableName)
 	if !ok {
-		table := NewTableWit(tableName, stmt)
+		table := NewTableWith(tableName, stmt)
 		db.AddTable(table)
 	} else {
 		if !stmt.GetIfExists() {
