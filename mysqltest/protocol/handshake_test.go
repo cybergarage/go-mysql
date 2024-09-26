42dbc858e01bc8a915954b44604982f98b64d554
Satoshi Konno <skonno@cybergarage.org>
2024-09-26 21:55:58 +0900

    Add TestHandshakeMessage()

---
+++ mysqltest/protocol/handshake_test.go
@@ -0,0 +1,42 @@
+// Copyright (C) 2024 The go-mysql Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protocol
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	"github.com/cybergarage/go-mysql/mysql/protocol"
+)
+
+//go:embed data/handshake001.hex
+var handshakeMsg001 string
+
+func TestHandshakeMessage(t *testing.T) {
+	for _, test := range []struct {
+		name string
+		data string
+	}{
+		{"handshake", handshakeMsg001},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := protocol.NewHandshakeWith(strings.NewReader(test.data))
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
