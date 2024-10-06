4b493a612094c1cc058fa388345d20f6c929839b
Satoshi Konno <skonno@cybergarage.org>
2024-10-06 20:22:03 +0900

    Update protocol.NewOKFromReader()

--- mysql/protocol/ok.go
+++ mysql/protocol/ok.go
@@ -28,15 +28,36 @@ const (
 // OK represents a MySQL OK packet.
 type OK struct {
 	*packet
-	header uint8
+	header           uint8
+	affectedRows     uint64
+	lastInsertID     uint64
+	status           uint16
+	warnings         uint16
+	info             string
+	sessionStateInfo string
 }
 
 // OKOption represents a MySQL OK packet option.
 type OKOption func(*OK) error
 
+// WithOKCapability returns a OKOption that sets the capability flag.
+func WithOKCapability(cap CapabilityFlag) OKOption {
+	return func(pkt *OK) error {
+		pkt.SetCapabilityEnabled(cap)
+		return nil
+	}
+}
+
 func newOKPacket(p *packet, opts ...OKOption) (*OK, error) {
 	pkt := &OK{
-		packet: p,
+		packet:           p,
+		header:           0,
+		affectedRows:     0,
+		lastInsertID:     0,
+		status:           0,
+		warnings:         0,
+		info:             "",
+		sessionStateInfo: "",
 	}
 	for _, opt := range opts {
 		if err := opt(pkt); err != nil {
@@ -70,11 +91,105 @@ func NewOKFromReader(reader io.Reader, opts ...OKOption) (*OK, error) {
 	if err != nil {
 		return nil, err
 	}
-	if (pkt.header != okPacketHeader) && (pkt.header != okPacketHeader) {
+	if (pkt.header != okPacketHeader) && (pkt.header != errPacketHeader) {
 		return nil, newErrInvalitHeader("OK", pkt.header)
 	}
 
+	// affectedRows
+	pkt.affectedRows, err = pkt.ReadLengthEncodedInt()
+	if err != nil {
+		return nil, err
+	}
+
+	// lastInsertID
+	pkt.lastInsertID, err = pkt.ReadLengthEncodedInt()
+	if err != nil {
+		return nil, err
+	}
+
+	if pkt.CapabilityFlags().IsEnabled(ClientProtocol41) {
+		// status
+		v, err := pkt.ReadInt2()
+		if err != nil {
+			return nil, err
+		}
+		pkt.status = uint16(v)
+
+		// warnings
+		pkt.warnings, err = pkt.ReadInt2()
+		if err != nil {
+			return nil, err
+		}
+	} else if pkt.CapabilityFlags().IsEnabled(ClientTransactions) {
+		// status
+		pkt.status, err = pkt.ReadInt2()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if pkt.CapabilityFlags().IsEnabled(ClientSessionTrack) {
+		// info
+		pkt.info, err = pkt.ReadLengthEncodedString()
+		if err != nil {
+			return nil, err
+		}
+		if pkt.Status().IsEnabled(StatusSessionStateChanged) {
+			// sessionStateInfo
+			pkt.sessionStateInfo, err = pkt.ReadLengthEncodedString()
+			if err != nil {
+				return nil, err
+			}
+		}
+	} else {
+		// info
+		pkt.info, err = pkt.ReadEOFTerminatedString()
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return pkt, err
+}
+
+// OK returns true if the packet is an OK packet.
+func (pkt *OK) OK() bool {
+	return pkt.header == okPacketHeader
+}
+
+// Err returns true if the packet is an ERR packet.
+func (pkt *OK) Err() bool {
+	return pkt.header == errPacketHeader
+}
+
+// AffectedRows returns the number of affected rows.
+func (pkt *OK) AffectedRows() uint64 {
+	return pkt.affectedRows
+}
+
+// LastInsertID returns the last insert ID.
+func (pkt *OK) LastInsertID() uint64 {
+	return pkt.lastInsertID
+}
+
+// Status returns the status flag.
+func (pkt *OK) Status() StatusFlag {
+	return StatusFlag(pkt.status)
+}
+
+// Warnings returns the number of warnings.
+func (pkt *OK) Warnings() uint16 {
+	return pkt.warnings
+}
+
+// Info returns the info.
+func (pkt *OK) Info() string {
+	return pkt.info
+}
+
+// SessionStateInfo returns the session state info.
+func (pkt *OK) SessionStateInfo() string {
+	return pkt.sessionStateInfo
 }
 
 // Bytes returns a byte sequence of the OK packet.
