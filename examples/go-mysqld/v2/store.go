03536b69499053a5c0856e72e66dcb89d76bfbf5
Satoshi Konno <skonno@cybergarage.org>
2024-10-14 12:47:05 +0900

    Update v2 example package

--- examples/go-mysqld/v2/store.go
+++ examples/go-mysqld/v2/store.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package vitess
+package v2
 
 import (
 	"github.com/cybergarage/go-mysql/mysql/plugins/vitess"
