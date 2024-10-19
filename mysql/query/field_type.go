ffdcf6ee56a08958a488386ca96544c8507ef886
Satoshi Konno <skonno@cybergarage.org>
2024-10-19 22:44:02 +0900

    Update query field type constants

--- mysql/query/field_type.go
+++ mysql/query/field_type.go
@@ -21,74 +21,74 @@ package query
 type FieldType uint8
 
 const (
-	// MysqlTypeDecimal represents a MYSQL_TYPE_DECIMAL type.
-	MysqlTypeDecimal FieldType = iota
-	// MysqlTypeTiny represents a MYSQL_TYPE_TINYINT type.
-	MysqlTypeTiny
-	// MysqlTypeShort represents a MYSQL_TYPE_SMALLINT type.
-	MysqlTypeShort
-	// MysqlTypeLong represents an INT type.
-	MysqlTypeLong
-	// MysqlTypeFloat represents a MYSQL_TYPE_FLOAT type.
-	MysqlTypeFloat
-	// MysqlTypeDouble represents a MYSQL_TYPE_DOUBLE type.
-	MysqlTypeDouble
-	// MysqlTypeNull represents a MYSQL_TYPE_NULL type.
-	MysqlTypeNull
-	// MysqlTypeTimestamp represents a MYSQL_TYPE_TIMESTAMP type.
-	MysqlTypeTimestamp
-	// MysqlTypeLonglong represents a MYSQL_TYPE_BIGINT type.
-	MysqlTypeLonglong
-	// MysqlTypeInt24 represents a MYSQL_TYPE_MEDIUMINT type.
-	MysqlTypeInt24
-	// MysqlTypeDate represents a MYSQL_TYPE_DATE type.
-	MysqlTypeDate
-	// MysqlTypeTime represents a MYSQL_TYPE_TIME type.
-	MysqlTypeTime
-	// MysqlTypeDatetime represents a MYSQL_TYPE_DATETIME type.
-	MysqlTypeDatetime
-	// MysqlTypeYear represents a MYSQL_TYPE_YEAR type.
-	MysqlTypeYear
-	// MysqlTypeNewdate represents a MYSQL_TYPE_NEWDATE type.
-	MysqlTypeNewdate
-	// MysqlTypeVarchar represents a MYSQL_TYPE_VARCHAR type.
-	MysqlTypeVarchar
-	// MysqlTypeBit represents a MYSQL_TYPE_BIT type.
-	MysqlTypeBit
-	// MysqlTypeTimestamp2 represents a MYSQL_TYPE_TIMESTAMP2 type.
-	MysqlTypeTimestamp2
-	// MysqlTypeDatetime2 represents a MYSQL_TYPE_DATETIME2 type.
-	MysqlTypeDatetime2
-	// MysqlTypeTime2 represents a MYSQL_TYPE_TIME2 type.
-	MysqlTypeTime2
-	// MysqlTypeTypedArray represents a MYSQL_TYPE_TYPED_ARRAY type.
-	MysqlTypeTypedArray
-	// MysqlTypeVector represents a MYSQL_TYPE_VECTOR type.
-	MysqlTypeVector = 242
-	// MysqlTypeInvalid represents an INVALID type.
-	MysqlTypeInvalid = 243
-	// MysqlTypeBool represents a MYSQL_TYPE_BOOL type.
-	MysqlTypeBool = 244
-	// MysqlTypeJSON represents a MYSQL_TYPE_JSON type.
-	MysqlTypeJSON = 245
-	// MysqlTypeNewdecimal represents a MYSQL_TYPE_NEWDECIMAL type.
-	MysqlTypeNewdecimal = 246
-	// MysqlTypeEnum represents an ENUM type.
-	MysqlTypeEnum = 247
-	// MysqlTypeSet represents a MYSQL_TYPE_SET type.
-	MysqlTypeSet = 248
-	// MysqlTypeTinyBlob represents a MYSQL_TYPE_TINYBLOB type.
-	MysqlTypeTinyBlob = 249
-	// MysqlTypeMediumBlob represents a MYSQL_TYPE_MEDIUMBLOB type.
-	MysqlTypeMediumBlob = 250
-	// MysqlTypeLongBlob represents a MYSQL_TYPE_LONGBLOB type.
-	MysqlTypeLongBlob = 251
-	// MysqlTypeBlob represents a MYSQL_TYPE_BLOB type.
-	MysqlTypeBlob = 252
-	// MysqlTypeVarString represents a MYSQL_TYPE_VAR_STRING type.
-	MysqlTypeVarString = 253
-	// MysqlTypeString represents a MYSQL_TYPE_STRING type.
-	MysqlTypeString = 254
-	// MysqlTypeGeometry represents a MYSQL_TYPE_GEOMETRY type.
-	MysqlTypeGeometry = 255
+	// MySQLTypeDecimal represents a MYSQL_TYPE_DECIMAL type.
+	MySQLTypeDecimal FieldType = iota
+	// MySQLTypeTiny represents a MYSQL_TYPE_TINYINT type.
+	MySQLTypeTiny
+	// MySQLTypeShort represents a MYSQL_TYPE_SMALLINT type.
+	MySQLTypeShort
+	// MySQLTypeLong represents an INT type.
+	MySQLTypeLong
+	// MySQLTypeFloat represents a MYSQL_TYPE_FLOAT type.
+	MySQLTypeFloat
+	// MySQLTypeDouble represents a MYSQL_TYPE_DOUBLE type.
+	MySQLTypeDouble
+	// MySQLTypeNull represents a MYSQL_TYPE_NULL type.
+	MySQLTypeNull
+	// MySQLTypeTimestamp represents a MYSQL_TYPE_TIMESTAMP type.
+	MySQLTypeTimestamp
+	// MySQLTypeLonglong represents a MYSQL_TYPE_BIGINT type.
+	MySQLTypeLonglong
+	// MySQLTypeInt24 represents a MYSQL_TYPE_MEDIUMINT type.
+	MySQLTypeInt24
+	// MySQLTypeDate represents a MYSQL_TYPE_DATE type.
+	MySQLTypeDate
+	// MySQLTypeTime represents a MYSQL_TYPE_TIME type.
+	MySQLTypeTime
+	// MySQLTypeDatetime represents a MYSQL_TYPE_DATETIME type.
+	MySQLTypeDatetime
+	// MySQLTypeYear represents a MYSQL_TYPE_YEAR type.
+	MySQLTypeYear
+	// MySQLTypeNewdate represents a MYSQL_TYPE_NEWDATE type.
+	MySQLTypeNewdate
+	// MySQLTypeVarchar represents a MYSQL_TYPE_VARCHAR type.
+	MySQLTypeVarchar
+	// MySQLTypeBit represents a MYSQL_TYPE_BIT type.
+	MySQLTypeBit
+	// MySQLTypeTimestamp2 represents a MYSQL_TYPE_TIMESTAMP2 type.
+	MySQLTypeTimestamp2
+	// MySQLTypeDatetime2 represents a MYSQL_TYPE_DATETIME2 type.
+	MySQLTypeDatetime2
+	// MySQLTypeTime2 represents a MYSQL_TYPE_TIME2 type.
+	MySQLTypeTime2
+	// MySQLTypeTypedArray represents a MYSQL_TYPE_TYPED_ARRAY type.
+	MySQLTypeTypedArray
+	// MySQLTypeVector represents a MYSQL_TYPE_VECTOR type.
+	MySQLTypeVector = 242
+	// MySQLTypeInvalid represents an INVALID type.
+	MySQLTypeInvalid = 243
+	// MySQLTypeBool represents a MYSQL_TYPE_BOOL type.
+	MySQLTypeBool = 244
+	// MySQLTypeJSON represents a MYSQL_TYPE_JSON type.
+	MySQLTypeJSON = 245
+	// MySQLTypeNewdecimal represents a MYSQL_TYPE_NEWDECIMAL type.
+	MySQLTypeNewdecimal = 246
+	// MySQLTypeEnum represents an ENUM type.
+	MySQLTypeEnum = 247
+	// MySQLTypeSet represents a MYSQL_TYPE_SET type.
+	MySQLTypeSet = 248
+	// MySQLTypeTinyBlob represents a MYSQL_TYPE_TINYBLOB type.
+	MySQLTypeTinyBlob = 249
+	// MySQLTypeMediumBlob represents a MYSQL_TYPE_MEDIUMBLOB type.
+	MySQLTypeMediumBlob = 250
+	// MySQLTypeLongBlob represents a MYSQL_TYPE_LONGBLOB type.
+	MySQLTypeLongBlob = 251
+	// MySQLTypeBlob represents a MYSQL_TYPE_BLOB type.
+	MySQLTypeBlob = 252
+	// MySQLTypeVarString represents a MYSQL_TYPE_VAR_STRING type.
+	MySQLTypeVarString = 253
+	// MySQLTypeString represents a MYSQL_TYPE_STRING type.
+	MySQLTypeString = 254
+	// MySQLTypeGeometry represents a MYSQL_TYPE_GEOMETRY type.
+	MySQLTypeGeometry = 255
 )
