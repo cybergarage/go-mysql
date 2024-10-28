59119beb5810e0d61e1d1ac1f78aa34c1800252f
Satoshi Konno <skonno@cybergarage.org>
2024-10-28 12:57:20 +0900

    Add protocol.Counter

---
+++ mysql/protocol/counter.go
@@ -0,0 +1,47 @@
+// Copyright (C) 2019 The go-mysql Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protocol
+
+import (
+	"math"
+)
+
+// Counter is a counter.
+type Counter struct {
+	count int32
+}
+
+// NewCounter returns a new counter.
+func NewCounter() *Counter {
+	return &Counter{
+		count: 0,
+	}
+}
+
+// NewCounterWith returns a new counter with the specified count.
+func NewCounterWith(count int32) *Counter {
+	return &Counter{
+		count: count,
+	}
+}
+
+// Inc increments the counter and returns the new value.
+func (counter *Counter) Inc() int32 {
+	if counter.count == math.MaxInt32 {
+		counter.count = 0
+	}
+	counter.count++
+	return counter.count
+}
