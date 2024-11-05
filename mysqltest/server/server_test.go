8252455646846ec4574dc0f7be1935535f019f96
Satoshi Konno <skonno@cybergarage.org>
2024-11-05 18:32:14 +0900

    Fix TestServer()

--- mysqltest/server/server_test.go
+++ mysqltest/server/server_test.go
@@ -25,7 +25,7 @@ var testQueries []string = []string{
 	"CREATE DATABASE IF NOT EXISTS ycsb",
 	"USE ycsb",
 	"CREATE TABLE usertable (YCSB_KEY VARCHAR(255) PRIMARY KEY, FIELD0 TEXT, FIELD1 TEXT, FIELD2 TEXT, FIELD3 TEXT, FIELD4 TEXT, FIELD5 TEXT, FIELD6 TEXT, FIELD7 TEXT, FIELD8 TEXT, FIELD9 TEXT)",
-	"DROP TABLE ycsb",
+	"DROP TABLE usertable",
 	"DROP DATABASE ycsb",
 }
 
@@ -61,7 +61,7 @@ func TestServer(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		defer rows.Close()
+		rows.Close()
 	}
 
 	err = server.Stop()
