699b17c237ceb6a5c28f51d8281d9701ddab0b6e
Satoshi Konno <skonno@cybergarage.org>
2024-11-14 23:33:59 +0900

    Update MemStore::Select()

--- examples/go-mysqld/server/store/memstore.go
+++ examples/go-mysqld/server/store/memstore.go
@@ -294,7 +294,7 @@ func (store *MemStore) Select(conn net.Conn, stmt query.Select) (sql.ResultSet, 
 				rowValues = append(rowValues, value)
 			}
 			rsRow := resultset.NewRow(
-				resultset.WithRowValues(rowValues),
+				resultset.WithRowObjects(rowValues),
 			)
 			rsRows = append(rsRows, rsRow)
 			rowIdx++
