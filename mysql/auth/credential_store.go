b3ac7ca8f71d6c1cbfc96a910589fcfb987003d8
Satoshi Konno <skonno@cybergarage.org>
2025-01-10 12:56:25 +0900

    Update auth.CredentialStore

--- mysql/auth/credential_store.go
+++ mysql/auth/credential_store.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	http://www.apache.org/licenses/LICENSE-2.0
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,8 +18,22 @@ import (
 	"github.com/cybergarage/go-authenticator/auth"
 )
 
-// CredentialAuthenticator is the credential authenticator.
-type CredentialStore = auth.CredentialStore
+// nolint: gosec
+const (
+	MySQLOldPassword = "mysql_old_password"
+
+	// MySQL: Native Authentication
+	// https://dev.mysql.com/doc/dev/mysql-server/8.4.2/page_protocol_connection_phase_authentication_methods_native_password_authentication.html
+
+	MySQLNativePassword = "mysql_native_password"
+
+	// MySQL: Caching_sha2_password information
+	// https://dev.mysql.com/doc/dev/mysql-server/8.4.2/page_caching_sha2_authentication_exchanges.html
+	MySQLCachingSHA2Password = "caching_sha2_password"
+)
 
 // CredentialAuthenticator is the interface for authenticating a client using credential.
 type CredentialAuthenticator = auth.CredentialAuthenticator
+
+// CredentialAuthenticator is the credential authenticator.
+type CredentialStore = auth.CredentialStore
