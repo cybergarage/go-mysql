edfa41d71aa6abe4947157ae4a462067dc59084f
Satoshi Konno <skonno@cybergarage.org>
2025-01-18 14:11:27 +0900

    Update protocol.Server::receive()

--- mysql/protocol/server.go
+++ mysql/protocol/server.go
@@ -164,22 +164,6 @@ func (server *Server) serve() error {
 	return nil
 }
 
-// GenerateHandshakeForConn retrun a handshake message for the specified connection.
-func (server *Server) GenerateHandshakeForConn(conn mysqlnet.Conn) (*Handshake, error) {
-	salt, err := auth.NewSalt(DefaultAuthPluginDataPartLen)
-	if err != nil {
-		return nil, err
-	}
-	return NewHandshake(
-		WithHandshakeCharacterSet(CharSetUTF8),
-		WithHandshakeCapability(server.Capability()),
-		WithHandshakeServerVersion(server.ServerVersion()),
-		WithHandshakeConnectionID(uint32(conn.ID())),
-		WithHandshakeAuthPluginData(salt),
-		WithHandshakeAuthPluginName(server.AuthPluginName()),
-	), nil
-}
-
 // receive handles client packets.
 func (server *Server) receive(netConn net.Conn) error { //nolint:gocyclo,maintidx
 	// MySQL: Connection Lifecycle
@@ -227,7 +211,22 @@ func (server *Server) receive(netConn net.Conn) error { //nolint:gocyclo,maintid
 
 	// Initial Handshake Packet
 
-	handshakeMsg, err := server.GenerateHandshakeForConn(conn)
+	generateHandshakeForConn := func(conn mysqlnet.Conn) (*Handshake, error) {
+		salt, err := auth.NewSalt(DefaultAuthPluginDataPartLen)
+		if err != nil {
+			return nil, err
+		}
+		return NewHandshake(
+			WithHandshakeCharacterSet(CharSetUTF8),
+			WithHandshakeCapability(server.Capability()),
+			WithHandshakeServerVersion(server.ServerVersion()),
+			WithHandshakeConnectionID(uint32(conn.ID())),
+			WithHandshakeAuthPluginData(salt),
+			WithHandshakeAuthPluginName(server.AuthPluginName()),
+		), nil
+	}
+
+	handshakeMsg, err := generateHandshakeForConn(conn)
 	if err != nil {
 		return err
 	}
