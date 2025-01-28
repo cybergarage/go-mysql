9cde7039253a4bd54d3bc931c69fb40e7c65df31
Satoshi Konno <skonno@cybergarage.org>
2025-01-28 12:33:32 +0900

    Add TestStmtPrepareResponsePacket()

---
+++ mysqltest/protocol/stmt_prepare_test.go
@@ -0,0 +1,79 @@
+// Copyright (C) 2024 The go-mysql Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protocol
+
+import (
+	"bytes"
+	_ "embed"
+	"testing"
+
+	"github.com/cybergarage/go-logger/log/hexdump"
+	"github.com/cybergarage/go-mysql/mysql/protocol"
+)
+
+func TestStmtPrepareResponsePacket(t *testing.T) {
+	type expected struct {
+		query string
+	}
+	for _, test := range []struct {
+		name string
+		expected
+	}{
+		{
+			"data/prepare-001.hex",
+			expected{
+				query: "select @@version_comment limit 1",
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			testData, err := testPackettFiles.ReadFile(test.name)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			testBytes, err := hexdump.NewBytesWithHexdumpBytes(testData)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			reader := bytes.NewReader(testBytes)
+
+			pkt, err := protocol.NewStmtPrepareFromReader(reader)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			// Compare the packet fields
+
+			if pkt.Query() != test.query {
+				t.Errorf("Query: expected %v, got %v", test.query, pkt.Query())
+			}
+
+			// Compare the packet bytes
+
+			msgBytes, err := pkt.Bytes()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			if !bytes.Equal(msgBytes, testBytes) {
+				HexdumpErrors(t, testBytes, msgBytes)
+			}
+		})
+	}
+}
