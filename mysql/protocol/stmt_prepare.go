d6a440e1d444a1723c927411ff9242a939744f77
Satoshi Konno <skonno@cybergarage.org>
2025-01-28 12:34:28 +0900

    Add protocol.NewStmtPrepareFromReader()

--- mysql/protocol/stmt_prepare.go
+++ mysql/protocol/stmt_prepare.go
@@ -14,6 +14,11 @@
 
 package protocol
 
+import (
+	"bytes"
+	"io"
+)
+
 // MySQL: COM_STMT_PREPARE
 // https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_com_stmt_prepare.html
 
@@ -44,7 +49,55 @@ func WithStmtPrepareQuery(query string) StmtPrepareOption {
 	}
 }
 
+// NewStmtPrepareFromReader reads a COM_STMT_PREPARE packet.
+func NewStmtPrepareFromReader(reader io.Reader, opts ...StmtPrepareOption) (*StmtPrepare, error) {
+	var err error
+
+	cmd, err := NewCommandFromReader(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = cmd.IsType(ComQuery); err != nil {
+		return nil, err
+	}
+
+	return NewStmtPrepareFromCommand(cmd, opts...)
+}
+
+// NewStmtPrepareFromCommand creates a new StmtPrepare from a Command.
+func NewStmtPrepareFromCommand(cmd Command, opts ...StmtPrepareOption) (*StmtPrepare, error) {
+	var err error
+
+	pkt := newStmtPrepareWithCommand(cmd, opts...)
+
+	payload := cmd.Payload()
+	reader := NewPacketReaderWith(bytes.NewBuffer(payload[1:]))
+
+	pkt.query, err = reader.ReadEOFTerminatedString()
+	if err != nil {
+		return nil, err
+	}
+
+	return pkt, nil
+}
+
 // Query returns the query string.
 func (stmt *StmtPrepare) Query() string {
 	return stmt.query
+}
+
+// Bytes returns the packet bytes.
+func (pkt *StmtPrepare) Bytes() ([]byte, error) {
+	w := NewPacketWriter()
+
+	if err := w.WriteCommandType(pkt); err != nil {
+		return nil, err
+	}
+
+	if _, err := w.WriteBytes([]byte(pkt.query)); err != nil {
+		return nil, err
+	}
+
+	return pkt.Command.Bytes()
 }
