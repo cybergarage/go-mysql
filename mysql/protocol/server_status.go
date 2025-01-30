efe7f05f2023350e8de1bb6d3cecb0793e5585dc
Satoshi Konno <skonno@cybergarage.org>
2025-01-30 13:27:20 +0900

    Add protocol.ServerStatus

---
+++ mysql/protocol/server_status.go
@@ -0,0 +1,35 @@
+// Copyright (C) 2025 The go-mysql Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protocol
+
+// ServerStatus represents a MySQL server status flag.
+type ServerStatus uint16
+
+const (
+	ServerStatusInTrans            ServerStatus = 1
+	ServerStatusAutocommit         ServerStatus = 2
+	ServerMoreResultsExists        ServerStatus = 8
+	ServerQueryNoGoodIndexUsed     ServerStatus = 16
+	ServerQueryNoIndexUsed         ServerStatus = 32
+	ServerStatusCursorExists       ServerStatus = 64
+	ServerStatusLastRowSent        ServerStatus = 128
+	ServerStatusDbDropped          ServerStatus = 256
+	ServerStatusNoBackslashEscapes ServerStatus = 512
+	ServerStatusMetadataChanged    ServerStatus = 1024
+	ServerQueryWasSlow             ServerStatus = 2048
+	ServerPsOutParams              ServerStatus = 4096
+	ServerStatusInTransReadonly    ServerStatus = 8192
+	ServerSessionStateChanged      ServerStatus = (1 << 14)
+)
