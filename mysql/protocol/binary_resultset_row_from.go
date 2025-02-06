d64941bc9dc4478d9341113d594ea48a80c99248
Satoshi Konno <skonno@cybergarage.org>
2025-02-07 00:31:04 +0900

    Add protocol.NewBinaryResultSetRowFrom()

---
+++ mysql/protocol/binary_resultset_row_from.go
@@ -0,0 +1,27 @@
+// Copyright (C) 2025 The go-mysql Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protocol
+
+// MySQL: Protocol::QueryResponse
+// https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_com_query.html
+// MySQL: Binary Protocol Resultset
+// https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_binary_resultset.html
+// Result Set Packets - MariaDB Knowledge Base
+// https://mariadb.com/kb/en/result-set-packets/
+
+// NewBinaryResultSetRowFrom creates a new BinaryResultSetRow from a TextResultSetRow.
+func NewBinaryResultSetRowFrom(txtRow *TextResultSetRow) (*BinaryResultSetRow, error) {
+	return nil, ErrNotSupported
+}
