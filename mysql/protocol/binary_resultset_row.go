22fc3ba6206a456d0d2983aa79d41bf8def8789a
Satoshi Konno <skonno@cybergarage.org>
2025-02-08 22:57:29 +0900

    Update protocol.BinaryResultSetRow

--- mysql/protocol/binary_resultset_row.go
+++ mysql/protocol/binary_resultset_row.go
@@ -14,11 +14,7 @@
 
 package protocol
 
-// MySQL: Protocol::QueryResponse
-// https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_com_query.html
 // MySQL: Binary Protocol Resultset
-// https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_binary_resultset.html
-// MySQL: Binary Protocol Resultset Row
 // https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_binary_resultset.html
 // Resultset row - MariaDB Knowledge Base
 // https://mariadb.com/kb/en/resultset-row/
@@ -28,55 +24,41 @@ type BinaryResultSetRowOption func(*BinaryResultSetRow)
 
 // BinaryResultSetRow represents a MySQL binary resultset row response packet.
 type BinaryResultSetRow struct {
-	*packet
-	columns []string
+	t     FieldType
+	bytes []byte
 }
 
-func newBinaryResultSetRowWithPacket(pkt *packet, opts ...BinaryResultSetRowOption) *BinaryResultSetRow {
+func newBinaryResultSetRowWithPacket(opts ...BinaryResultSetRowOption) *BinaryResultSetRow {
 	row := &BinaryResultSetRow{
-		packet:  pkt,
-		columns: []string{},
+		t:     0,
+		bytes: nil,
 	}
 	row.SetOptions(opts...)
 	return row
 }
 
-// WithBinaryResultSetRowColmunCount returns a binary resultset row option to set the column count.
-func WithBinaryResultSetRowColmunCount(c uint64) BinaryResultSetRowOption {
-	return func(pkt *BinaryResultSetRow) {
-		pkt.columns = make([]string, c)
+// WithBinaryResultSetRowType returns a binary resultset row option to set the type.
+func WithwBinaryResultSetRowType(t FieldType) BinaryResultSetRowOption {
+	return func(row *BinaryResultSetRow) {
+		row.t = t
 	}
 }
 
-// WithBinaryResultSetRowColmuns returns a binary resultset row option to set the columns.
-func WithBinaryResultSetRowColmuns(columns []string) BinaryResultSetRowOption {
-	return func(pkt *BinaryResultSetRow) {
-		pkt.columns = columns
+// WithBinaryResultSetRowBytes returns a binary resultset row option to set the bytes.
+func WithwBinaryResultSetRowBytes(b []byte) BinaryResultSetRowOption {
+	return func(row *BinaryResultSetRow) {
+		row.bytes = b
 	}
 }
 
 // NewBinaryResultSetRow returns a new BinaryResultSetRow.
 func NewBinaryResultSetRow(opts ...BinaryResultSetRowOption) *BinaryResultSetRow {
-	return newBinaryResultSetRowWithPacket(newPacket(), opts...)
+	return newBinaryResultSetRowWithPacket(opts...)
 }
 
 // NewBinaryResultSetRowFromReader returns a new BinaryResultSetRow from the reader.
 func NewBinaryResultSetRowFromReader(reader *PacketReader, opts ...BinaryResultSetRowOption) (*BinaryResultSetRow, error) {
-	pktReader, err := NewPacketHeaderWithReader(reader)
-	if err != nil {
-		return nil, err
-	}
-
-	row := newBinaryResultSetRowWithPacket(pktReader, opts...)
-	columnCount := len(row.columns)
-	for n := 0; n < columnCount; n++ {
-		column, err := row.ReadColumn()
-		if err != nil {
-			return nil, err
-		}
-		row.columns[n] = column
-	}
-
+	row := newBinaryResultSetRowWithPacket(opts...)
 	return row, nil
 }
 
@@ -87,29 +69,12 @@ func (row *BinaryResultSetRow) SetOptions(opts ...BinaryResultSetRowOption) {
 	}
 }
 
-// ReadColumn reads a column.
-func (row *BinaryResultSetRow) ReadColumn() (string, error) {
-	return row.ReadLengthEncodedString()
+// Type returns the type.
+func (row *BinaryResultSetRow) Type() FieldType {
+	return row.t
 }
 
-// Columns returns the columns.
-func (row *BinaryResultSetRow) Columns() []any {
-	columns := make([]any, len(row.columns))
-	for n, column := range row.columns {
-		columns[n] = column
-	}
-	return columns
-}
-
-// Bytes returns the packet bytes.
-func (row *BinaryResultSetRow) Bytes() ([]byte, error) {
-	w := NewPacketWriter()
-	for _, column := range row.columns {
-		err := w.WriteLengthEncodedString(column)
-		if err != nil {
-			return nil, err
-		}
-	}
-	row.SetPayload(w.Bytes())
-	return row.packet.Bytes()
+// Bytes returns the bytes.
+func (row *BinaryResultSetRow) Bytes() []byte {
+	return row.bytes
 }
