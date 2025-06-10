5d9716ef934c46186d5158e5b4dfb4b9f9ece0ff
Satoshi Konno <skonno@cybergarage.org>
2025-06-10 23:32:52 +0900

    Update TestHandshakeResponsePacket()

--- mysqltest/protocol/handshake_response_test.go
+++ mysqltest/protocol/handshake_response_test.go
@@ -109,20 +109,20 @@ func TestHandshakeResponsePacket(t *testing.T) {
 		// 		zstdLevel:  0,
 		// 	},
 		// },
-		// {
-		// 	"data/handshake-response-sysbench-1.0.20-01.hex",
-		// 	expected{
-		// 		capFlags:   protocol.Capability(0x00bfaa8d),
-		// 		maxPkt:     0,
-		// 		charSet:    0,
-		// 		username:   "",
-		// 		authRes:    nil,
-		// 		database:   "",
-		// 		pluginName: "",
-		// 		attrs:      map[string]string{},
-		// 		zstdLevel:  0,
-		// 	},
-		// },
+		{
+			"data/handshake-response-sysbench-1.0.20-01.hex",
+			expected{
+				capFlags:   protocol.Capability(0x00bfaa8d),
+				maxPkt:     0,
+				charSet:    0,
+				username:   "",
+				authRes:    nil,
+				database:   "",
+				pluginName: "",
+				attrs:      map[string]string{},
+				zstdLevel:  0,
+			},
+		},
 		// {
 		// 	"data/handshake-response-sysbench-1.0.20-02.hex",
 		// 	expected{
