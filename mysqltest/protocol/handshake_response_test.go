ed7c5405a3426a6f15447d752f6b471ba2ebb02e
Satoshi Konno <skonno@cybergarage.org>
2025-06-19 21:32:10 +0900

    Update TestHandshakeResponsePacket()

--- mysqltest/protocol/handshake_response_test.go
+++ mysqltest/protocol/handshake_response_test.go
@@ -95,34 +95,34 @@ func TestHandshakeResponsePacket(t *testing.T) {
 				zstdLevel: 0,
 			},
 		},
-		// {
-		// 	"data/handshake-response-sysbench-1.0.20-01.hex",
-		// 	expected{
-		// 		capFlags:   protocol.Capability(0x00bfaa8d),
-		// 		maxPkt:     0,
-		// 		charSet:    0,
-		// 		username:   "",
-		// 		authRes:    nil,
-		// 		database:   "",
-		// 		pluginName: "",
-		// 		attrs:      map[string]string{},
-		// 		zstdLevel:  0,
-		// 	},
-		// },
-		// {
-		// 	"data/handshake-response-sysbench-1.0.20-02.hex",
-		// 	expected{
-		// 		capFlags:   protocol.Capability(0x00bfaa8d),
-		// 		maxPkt:     0,
-		// 		charSet:    0,
-		// 		username:   "",
-		// 		authRes:    nil,
-		// 		database:   "",
-		// 		pluginName: "",
-		// 		attrs:      map[string]string{},
-		// 		zstdLevel:  0,
-		// 	},
-		// },
+		{
+			"data/handshake-response-sysbench-1.0.20-01.hex",
+			expected{
+				capFlags:   protocol.Capability(0x00bfaa8d),
+				maxPkt:     0,
+				charSet:    0,
+				username:   "",
+				authRes:    nil,
+				database:   "",
+				pluginName: "",
+				attrs:      map[string]string{},
+				zstdLevel:  0,
+			},
+		},
+		{
+			"data/handshake-response-sysbench-1.0.20-02.hex",
+			expected{
+				capFlags:   protocol.Capability(0x00bfaa8d),
+				maxPkt:     0,
+				charSet:    0,
+				username:   "",
+				authRes:    nil,
+				database:   "",
+				pluginName: "",
+				attrs:      map[string]string{},
+				zstdLevel:  0,
+			},
+		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
 			testData, err := testEmbedPacketFiles.ReadFile(test.name)
